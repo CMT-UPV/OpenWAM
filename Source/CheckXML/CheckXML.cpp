5d8967292a0152ae68b6f58ad4c4bfb3143ce458
Luis Miguel García-Cuevas González <luismiguelgcg@gmail.com>
2014-05-28 17:45:49 +0200

    GetXMLArea, Pressure, Speed and Temperature.

--- Source/CheckXML/CheckXML.cpp
+++ Source/CheckXML/CheckXML.cpp
@@ -74,22 +74,143 @@ int CountNodes(xml_node node, const char* label) {
 }
 
 
-double GetXMLLength(const xml_node& node, const std::string& name,
-                      const std::string& unit)
+double GetXMLArea(const xml_node& node, const std::string& name)
 {
+	xml_node unit_node = node.child("Units");
+	std::string unit = unit_node.attribute("Length").value();
 	double x = GetAttributeAsDouble(node, name.c_str());
-	return to_meters(x, unit);
+	return to_square_metres(x, unit);
 }
 
 
 double GetXMLLength(const xml_node& node, const std::string& name)
 {
-	std::string unit = node.attribute("unit").value();
-	return GetXMLLength(node, name, unit);
+	xml_node unit_node = node.child("Units");
+	std::string unit = unit_node.attribute("Length").value();
+	double x = GetAttributeAsDouble(node, name.c_str());
+	return to_metres(x, unit);
 }
 
 
-double to_meters(const double& x, const std::string& unit)
+double GetXMLPressure(const xml_node& node, const std::string& name)
+{
+	xml_node unit_node = node.child("Units");
+	std::string unit = unit_node.attribute("Pressure").value();
+	double x = GetAttributeAsDouble(node, name.c_str());
+	return to_bar(x, unit);
+}
+
+
+double GetXMLSpeed(const xml_node& node, const std::string& name)
+{
+	xml_node unit_node = node.child("Units");
+	std::string unit = unit_node.attribute("Speed").value();
+	double x = GetAttributeAsDouble(node, name.c_str());
+	return to_m_s(x, unit);
+}
+
+
+double GetXMLTemperature(const xml_node& node, const std::string& name)
+{
+	xml_node unit_node = node.child("Units");
+	std::string unit = unit_node.attribute("Speed").value();
+	double x = GetAttributeAsDouble(node, name.c_str());
+	return to_celsius(x, unit);
+}
+
+
+double to_bar(const double& x, const std::string& unit)
+{
+	if (unit == ""){
+		return x;
+	}
+	else if (unit == "bar"){
+		return x;
+	}
+	else if (unit == "atm") {
+		return x / 1.01325;
+	}
+	else if (unit == "mbar"){
+		return x / 1000.;
+	}
+	else if (unit == "Pa"){
+		return x / 1e5;
+	}
+	else if (unit == "hPa") {
+		return x / 1000.;
+	}
+	else if (unit == "kPa") {
+		return x / 100.;
+	}
+	else if (unit == "MPa") {
+		return x * 10.;
+	}
+	else if (unit == "PSI") {
+		return x * 0.0689476;
+	}
+	else {
+		std::cout << "ERROR: Unit unknown" << std::endl;
+		std::cout << "       UNIT: " << unit << std::endl;
+		std::cout << "       Assuming bar..." << std::endl;
+		return x;
+	}
+}
+
+
+double to_celsius(const double& x, const std::string& unit)
+{
+	if (unit == ""){
+		return x;
+	}
+	else if (unit == "C"){
+		return x;
+	}
+	else if (unit == "ºC") {
+		return x;
+	}
+	else if (unit == "°C") {
+		return x;
+	}
+	else if (unit == "℃") {
+		return x;
+	}
+	else if (unit == "K") {
+		return x - 273.15;
+	}
+	else if (unit == "F") {
+		return (x - 32.) * 5. / 9.;
+	}
+	else if (unit == "ºF") {
+		return (x - 32.) * 5. / 9.;
+	}
+	else if (unit == "°F") {
+		return (x - 32.) * 5. / 9.;
+	}
+	else if (unit == "℉") {
+		return (x - 32.) * 5. / 9.;
+	}
+	else if (unit == "R")
+	{
+		return (x - 491.67) * 5. / 9.;
+	}
+	else if (unit == "ºR")
+	{
+		return (x - 491.67) * 5. / 9.;
+	}
+	else if (unit == "°R")
+	{
+		return (x - 491.67) * 5. / 9.;
+	}
+	else {
+		std::cout << "ERROR: Unit unknown" << std::endl;
+		std::cout << "       UNIT: " << unit << std::endl;
+		std::cout << "       Assuming ºC..." << std::endl;
+		return x;
+	}
+}
+
+
+double to_metres(const double& x, const std::string& unit)
 {
 	if (unit == ""){
 		return x;
@@ -112,7 +233,155 @@ double to_meters(const double& x, const std::string& unit)
 	else {
 		std::cout << "ERROR: Unit unknown" << std::endl;
 		std::cout << "       UNIT: " << unit << std::endl;
-		std::cout << "       Assuming meters..." << std::endl;
+		std::cout << "       Assuming m..." << std::endl;
+		return x;
+	}
+}
+
+
+double to_m_s(const double& x, const std::string& unit)
+{
+	if (unit == ""){
+		return x;
+	}
+	else if (unit == "m / s"){
+		return x;
+	}
+	else if (unit == "m/s"){
+		return x;
+	}
+	else if (unit == "km / hr") {
+		return x / 3.6;
+	}
+	else if (unit == "km/hr") {
+		return x / 3.6;
+	}
+	else if (unit == "km / h") {
+		return x / 36.;
+	}
+	else if (unit == "km/h") {
+		return x / 36.;
+	}
+	else if (unit == "mph"){
+		return x * 1609.34 / 3600.;
+	}
+	else if (unit == "MPH"){
+		return x * 1609.34 / 3600.;
+	}
+	else if (unit == "kn"){
+		return x * 0.514444;
+	}
+	else if (unit == "kt") {
+		return x * 0.514444;
+	}
+	else if (unit == "NMPH") {
+		return x * 0.514444;
+	}
+	else if (unit == "nmph") {
+		return x * 0.514444;
+	}
+	else {
+		std::cout << "ERROR: Unit unknown" << std::endl;
+		std::cout << "       UNIT: " << unit << std::endl;
+		std::cout << "       Assuming m / s..." << std::endl;
+		return x;
+	}
+}
+
+
+double to_square_metres(const double& x, const std::string& unit)
+{
+	if (unit == ""){
+		return x;
+	}
+	else if (unit == "m ** 2"){
+		return x;
+	}
+	else if (unit == "m**2") {
+		return x;
+	}
+	else if (unit == "m ^ 2") {
+		return x;
+	}
+	else if (unit == "m^2") {
+		return x;
+	}
+	else if (unit == "mm ** 2") {
+		return x * 1E-6;
+	}
+	else if (unit == "mm**2") {
+		return x * 1E-6;
+	}
+	else if (unit == "mm ^ 2") {
+		return x * 1E-6;
+	}
+	else if (unit == "mm^2") {
+		return x * 1E-6;
+	}
+	else if (unit == "cm ** 2") {
+		return x * 1E-4;
+	}
+	else if (unit == "cm**2") {
+		return x * 1E-4;
+	}
+	else if (unit == "cm ^ 2") {
+		return x * 1E-4;
+	}
+	else if (unit == "cm^2") {
+		return x * 1E-4;
+	}
+	else if (unit == "dm ** 2") {
+		return x * 1E-2;
+	}
+	else if (unit == "dm**2") {
+		return x * 1E-2;
+	}
+	else if (unit == "dm ^ 2") {
+		return x * 1E-2;
+	}
+	else if (unit == "dm^2") {
+		return x * 1E-2;
+	}
+	else if (unit == "inch ** 2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "inch**2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "inch ^ 2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "inch^2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "in ** 2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "in**2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "in ^ 2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "in^2") {
+		return x * 0.00064516;
+	}
+	else if (unit == "square in") {
+		return x * 0.00064516;
+	}
+	else if (unit == "sq inches") {
+		return x * 0.00064516;
+	}
+	else if (unit == "sq inch") {
+		return x * 0.00064516;
+	}
+	else if (unit == "sq in") {
+		return x * 0.00064516;
+	}
+	else {
+		std::cout << "ERROR: Unit unknown" << std::endl;
+		std::cout << "       UNIT: " << unit << std::endl;
+		std::cout << "       Assuming m / s..." << std::endl;
 		return x;
 	}
 }
--- Source/CheckXML/CheckXML.h
+++ Source/CheckXML/CheckXML.h
@@ -25,17 +25,15 @@ int CountNodes(xml_node node, const char* label);
 
 
 /**
- * @brief Gets an attribute as a length.
+ * @brief Gets an attribute as an area.
  * 
- * Gets an attribute as a length.
+ * Gets an attribute as an area.
  * 
  * @param node XML node.
  * @param name Attribute name.
- * @param unit Original unit.
- * @return Length. [m]
+ * @return Length. [m ** 2]
  */
-double GetXMLLength(const xml_node& node, const std::string& name,
-                    const std::string& unit);
+double GetXMLArea(const xml_node& node, const std::string& name);
 
 
 /**
@@ -51,14 +49,98 @@ double GetXMLLength(const xml_node& node, const std::string& name);
 
 
 /**
- * @brief Converts a given value from some unit to meters.
+ * @brief Gets an attribute as a pressure.
  * 
- * Converts a given value from some unit to meters.
+ * Gets an attribute as a pressure.
+ * 
+ * @param node XML node.
+ * @param name Attribute name.
+ * @return Pressure. [bar]
+ */
+double GetXMLPressure(const xml_node& node, const std::string& name);
+
+
+/**
+ * @brief Gets an attribute as a speed.
+ * 
+ * Gets an attribute as a speed.
+ * 
+ * @param node XML node.
+ * @param name Attribute name.
+ * @return Speed. [m / s]
+ */
+double GetXMLSpeed(const xml_node& node, const std::string& name);
+
+
+/**
+ * @brief Gets an attribute as a temperature.
+ * 
+ * Gets an attribute as a temperature.
+ * 
+ * @param node XML node.
+ * @param name Attribute name.
+ * @return Temperature. [ºC]
+ */
+double GetXMLTemperature(const xml_node& node, const std::string& name);
+
+
+/**
+ * @brief Converts a given pressure from some unit to bar.
+ * 
+ * Converts a given pressure from some unit to bar.
+ * 
+ * @param x Pressure to convert.
+ * @param unit Original unit.
+ * @return The pressure in bar. [bar]
+ */
+double to_bar(const double& x, const std::string& unit);
+
+
+/**
+ * @brief Converts a given temperature from some unit to celsius.
+ * 
+ * Converts a given temperature from some unit to celsius.
+ * 
+ * @param x Temperature to convert.
+ * @param unit Original unit.
+ * @return The temperature. [ºC]
+ */
+double to_celsius(const double& x, const std::string& unit);
+
+
+/**
+ * @brief Converts a given length from some unit to metres.
+ * 
+ * Converts a given length from some unit to metres.
  * 
  * @param x Size to convert.
  * @param unit Original unit.
- * @return The size in meters. [m]
+ * @return The size in metres. [m]
  */
-double to_meters(const double& x, const std::string& unit);
+double to_metres(const double& x, const std::string& unit);
+
+
+/**
+ * @brief Converts a given speed from some unit to metres per second.
+ * 
+ * Converts a given speed from some unit to metres per second.
+ * 
+ * @param x Speed to convert.
+ * @param unit Original unit.
+ * @return The speed in metres per second. [m / s]
+ */
+double to_m_s(const double& x, const std::string& unit);
+
+
+/**
+ * @brief Converts a given area from some unit to square metres.
+ * 
+ * Converts a given area from some unit to square metres.
+ * 
+ * @param x Area to convert.
+ * @param unit Original unit.
+ * @return The area in square metres. [m ** 2]
+ */
+double to_square_metres(const double& x, const std::string& unit);
 
 #endif
