0ddd132b2851944acefd1d0794f5b1b6973b25c4
Luis Miguel García-Cuevas González <luismiguelgcg@gmail.com>
2015-11-07 17:02:55 +0100

    More accuracy for Runiversal, Pi...
    More accuracy for Runiversal, Pi and StephanBoltzmann constants, as well
    as more comments in Constantes.h.

--- Source/Includes/Constantes.h
+++ Source/Includes/Constantes.h
@@ -58,37 +58,40 @@
 #include <vcl.h>
 #endif
 //---------------------------------------------------------------------------
-const double Runiversal = 8314.4;
-const double R = 287.;
-const double RBurnt = 285.4;
-const double RAir = 287.;
-const double RO2 = 259.825;
-const double RCO2 = 188.9207;
-const double RH2O = 461.398;
-const double RN2 = 296.837;
-const double RArgon = 208.12;
-const double PMO2 = 32;
-const double PMCO2 = 44.01;
-const double PMH2O = 18.02;
-const double PMN2 = 28.01;
-const double PMArgon = 39.95;
-const double PMCarbono = 12.01;
-const double PMNO2 = 46;
-const double PMNO = 30;
-const double PMCO = 28.01;
+const double Runiversal = 8314.4598; ///< Universal gas constant. [J / (kg * K)]
+const double R = 287.; ///< Air gas constant. [J / (kg * K)]
+const double RBurnt = 285.4; ///< Burnt gases gas constant. [J / (kg * K)]
+const double RAir = 287.; ///< Air gas constant. [J / (kg * K)]
+const double RO2 = 259.825; ///< O2 gas constant. [J / (kg * K)]
+const double RCO2 = 188.9207; ///< CO2 gas constant. [J / (kg * K)]
+const double RH2O = 461.398; ///< H2O gas constant. [J / (kg * K)]
+const double RN2 = 296.837; ///< N2 gas constant. [J / (kg * K)]
+const double RArgon = 208.12; ///< Ar gas constant. [J / (kg * K)]
+const double PMO2 = 32; ///< O2 molar mass. [g / mol]
+const double PMCO2 = 44.01; ///< CO2 molar mass. [g / mol]
+const double PMH2O = 18.02; ///< H2O molar mass. [g / mol]
+const double PMN2 = 28.01; ///< N2 molar mass. [g / mol]
+const double PMArgon = 39.95; ///< Ar molar mass. [g / mol]
+const double PMCarbono = 12.01; ///< C molar mass. [g / mol]
+const double PMNO2 = 46; ///< NO2 molar mass. [g / mol]
+const double PMNO = 30; ///< No molar mass. [g / mol]
+const double PMCO = 28.01; ///< CO molar mass. [g / mol]
 const double PMDiesel = 148.4; // Diesel C10.8H18.7 , los HC mantienen la misma relacion C/H y le damos
 const double PMHC = 55.04;     // la composici�n C4H6.93
 const double RDiesel = 55.95;
-const double PMGasoline = 114.8;    // Gasoline molar mass
+const double PMGasoline = 114.8; ///< Gasoline molar mass. [g / mol]
 const double RGasoline = 72.42;
 
-const double Pi = 3.1415927;
-const double Gamma = 1.4;
-const double ARef = 343.11;
+/// Ratio between the length of a circunference and its diameter.
+const double Pi = 3.141592653589793;
+const double Gamma = 1.4; ///< Reference specific heat capacities ratio. [-]
+const double ARef = 343.11; ///< Reference speed of sound. [m / s]
 const double TRef = 292.99271; ///< Reference temperature. [K]
 const double ARef2 = 117724.4721;
 const double PRef = 1.0; ///< Reference pressure. [bar]
-const double StephanBoltzmann = 5.67e-8; // W/m2 K4
+
+/// Stefan-Boltzmann constant. [W / m ** 2 / K ** 4]
+const double StephanBoltzmann = 5.670367e-8;
 
 const double unBarToPa = 1e5;
 const double unPaToBar = 1e-5;
